a7548b8876bdbb9974a89e0091be2d204ec86ed7
Gabriel Timba <gabrieltimba@hotmail.com>
2020-03-27 20:01:31 +0300

    backend aplication

---
+++ backend/src/controllers/ongController.js
@@ -0,0 +1,26 @@
+const crypto=require('crypto');
+const connection=require('../database/connection');
+
+module.exports ={
+
+    async index(req,res) {
+        const ongs= await connection('ongs').select('*');
+        return res.json(ongs);
+    },
+
+    async create(req,res){
+        const {name,emaily,whatsapp,city,uf}=req.body;
+        const id =crypto.randomBytes(4).toString('HEX');
+
+        await connection('ongs').insert({
+            id,
+            name,
+            emaily, 
+            whatsapp,
+            city,
+            uf
+        });
+
+        return res.json({id});
+    }
+}
